f60ee0267fea42c860852089b7f5fbbbb2c6050e
ilaria <ilariac@kth.se>
2025-12-30 14:58:35 +0100

    adding simulation helper file

---
+++ utils/Sim_helper.py
@@ -0,0 +1,36 @@
+import json
+import brian2 as b2
+# ---------------------------------------------------
+def get_input_config(idx = None, json_file_name = None):
+    if idx == None:
+        idx = 0
+
+    if json_file_name == None:
+        raise ValueError("Plese, specify the json_file_name containing the input configuration parameters")    
+    
+    with open(json_file_name, 'r') as file:
+        data = json.load(file)
+
+        return data[0]
+
+def get_syn_info(idx = None, json_file_name = None):
+    if idx == None:
+        idx = 0
+
+    if json_file_name == None:
+        raise ValueError("Plese, specify the json_file_name containing the model parameters")   
+    with open(json_file_name, 'r') as file:
+        data = json.load(file)
+
+    Qe = data[0][idx]['model']['Q_e'] * b2.nS
+    Qi = data[0][idx]['model']['Q_i'] * b2.nS
+    
+    return Qe, Qi
+
+
+
+
+
+
+
+
