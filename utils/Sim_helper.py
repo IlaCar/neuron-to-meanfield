2737bf9b955cf021503adf8f321f63ebde318185
ilaria <ilariac@kth.se>
2026-04-24 10:09:04 +0200

    adding func to load_sim_data

--- utils/Sim_helper.py
+++ utils/Sim_helper.py
@@ -1,6 +1,8 @@
 import json
 import brian2 as b2
 import h5py
+import os
+import pandas as pd
 
 # ---------------------------------------------------
 def get_input_config(idx = None, json_file_name = None):
@@ -76,7 +78,40 @@ def load_spike_data(fname):
         data["spikes"] = spikes
 
     return data
-
+    
+# -------------------- #
+def load_sim_data(folder_path, model_type="NN"):
+    """
+    Parses h5 files in a folder and returns a list of results.
+    model_type: "NN" (collects avg and std) or "MF" (collects avg)
+    """
+    results = []
+    
+    # Iterate through all files in the folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".h5") and "exc_" in filename:
+            # Parse exc and inh values from filename: sim_data_exc_10_inh_5.h5
+            parts = filename.replace('.h5', '').split('_')
+            exc = float(parts[3])
+            inh = float(parts[5])
+            
+            with h5py.File(os.path.join(folder_path, filename), "r") as f:
+                # Extract stats for both populations
+                res = {
+                    'exc': exc, 
+                    'inh': inh, 
+                    'fs_avg': f['stats']['FS_avg_freq'][()],
+                    'rs_avg': f['stats']['RS_avg_freq'][()]
+                }
+                
+                # Extract Standard Deviations only if it's the NN model
+                if model_type == "NN":
+                    res['fs_std'] = f['stats']['FS_std_freq'][()]
+                    res['rs_std'] = f['stats']['RS_std_freq'][()]
+                
+                results.append(res)
+                
+    return pd.DataFrame(results)
 
 
 
